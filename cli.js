3be104f734fe5a67302c483d9b32ae96a9691ad2
Filipe Linhares <lip.linhares@gmail.com>
2017-02-12 02:26:50 -0200

    Improves parse and add fetchTracking function

--- cli.js
+++ cli.js
@@ -1,38 +1,41 @@
 #!/usr/bin/env node
 'use strict';
 
-var got = require('got');
-var chalk = require('chalk');
-var strl = require('string-length');
-var repeat = require('repeating');
-var meow = require('meow');
-var storage = require('./storage');
-var updateNotifier = require('update-notifier');
-var CODE_REGEX = /[a-z]{2}[0-9]{9}[a-z]{2}/ig;
+const got = require('got');
+const chalk = require('chalk');
+const strl = require('string-length');
+const repeat = require('repeating');
+const meow = require('meow');
+const storage = require('./storage');
+const updateNotifier = require('update-notifier');
+const CODE_REGEX = /[a-z]{2}[0-9]{9}[a-z]{2}/ig;
 
-var command;
+const CORREIOS_SERVICE_URL = 'https://correios-tracking.now.sh/';
 
-var cli = meow([
-  'Como usar:',
-  '  $ onde-ta RE108441783BR',
-  '',
-  '  Salvar e visualizar códigos de rastreio',
-  '  $ onde-ta RE108441783BR --save batman',
-  '  $ onde-ta batman',
-  '',
-  '  Remover um código salvo',
-  '  $ onde-ta --remove batman',
-  '  $ onde-ta --clear',
-  '',
-  '  Listar todos os códigos salvos',
-  '  $ onde-ta --list',
-  '',
-  ' Opções',
-  '  -s, --save     Salva um código de rastreio com nome',
-  '  -r, --remove   Remove o código selecionado',
-  '  -c, --clear    Remove todos os códigos salvos',
-  '  -l, --list     Listar todos os códigos salvos com um nome'
-].join('\n'),
+let command;
+const bold = chalk.bold;
+
+const cli = meow(`
+  Como usar:
+    $ onde-ta RE108441783BR
+
+    Salvar e visualizar códigos de rastreio
+    $ onde-ta RE108441783BR --save batman
+    $ onde-ta batman
+
+    Remover um código salvo
+    $ onde-ta --remove batman
+    $ onde-ta --clear
+
+    Listar todos os códigos salvos
+    $ onde-ta --list
+
+   Opções
+    -s, --save     Salva um código de rastreio com nome
+    -r, --remove   Remove o código selecionado
+    -c, --clear    Remove todos os códigos salvos
+    -l, --list     Listar todos os códigos salvos com um nome
+`,
   {
     alias: {
       s: 'save',
@@ -44,53 +47,77 @@ var cli = meow([
 
 updateNotifier({pkg: cli.pkg}).notify();
 
-if (!CODE_REGEX.test(cli.input[0]) && cli.input[0]) {
-  command = storage.get(cli.input[0]);
-} else if (cli.input[0]) {
-  command = cli.input[0];
-} else if (cli.flags.clear) {
-  storage.clear();
-} else if (cli.flags.remove) {
-  storage.del(cli.flags.remove);
-} else if (cli.flags.list) {
-  storage.list();
-} else {
-  cli.showHelp();
+function run() {
+  if (!CODE_REGEX.test(cli.input[0]) && cli.input[0]) {
+    command = storage.get(cli.input[0]);
+  } else if (cli.input[0]) {
+    command = cli.input[0];
+  } else if (cli.flags.clear) {
+    storage.clear();
+  } else if (cli.flags.remove) {
+    storage.del(cli.flags.remove);
+  } else if (cli.flags.list) {
+    storage.list();
+  } else {
+    cli.showHelp();
+  }
+
+  if (cli.flags.save) {
+    storage.save(cli.flags.save, cli.input[0]);
+  }
 }
 
-if (cli.flags.save) {
-  storage.save(cli.flags.save, cli.input[0]);
-}
+run(cli)
 
 function parse(data) {
-  var longestLine = Math.max(strl(data.data), strl(data.local), strl(data.acao), strl(data.detalhes));
+  // `data.detalhe` is optional
+  data.detalhe = data.detalhe || '';
 
-  // 14 is the size of the rest of the texts
-  var bottomSize = longestLine + 14;
-  var topSize = bottomSize - strl(data.data) - 2;
+  // Check for the longest line
+  const longestLine = Math.max.apply(Math, [data.data, data.local, data.descricao, data.detalhe, `${chalk.bold(data.local)} - ${chalk.bold(data.cidade)}, ${chalk.bold(data.uf)}`].map(strl));
 
-  var output = [
-    chalk.yellow('┌ ') + chalk.bold(data.data) + ' ' + chalk.yellow(repeat('─', topSize)) + chalk.yellow('┐'),
-    ' ⇢ Local:    ' + chalk.bold(data.local),
-    ' ⇢ Ação:     ' + chalk.bold(data.acao),
-    ' ⇢ Detalhes: ' + chalk.bold(data.detalhes),
-    chalk.yellow('└') + chalk.yellow(repeat('─', bottomSize)) + chalk.yellow('┘')
-  ];
 
-  return console.log(output.join('\n'));
+  const bottomSize = longestLine + 2;
+  const topSize = bottomSize - (strl(data.data) + strl(data.hora)) - 4;
+
+  const details = (data.detalhe) ?
+        `${data.descricao}
+  ┆      ${data.detalhe}`
+                :
+                  data.descricao
+                ;
+
+  const output = `
+  ├┈ • ┌ ${bold(data.data)} - ${bold(data.hora)} ${repeat('─', topSize)} ┐
+  ┆      ${bold(data.local)} - ${bold(data.cidade)}, ${bold(data.uf)}
+  ┆
+  ┆      ${details}
+  ┆    └ ${repeat('─', bottomSize)} ┘
+  ┆`;
+
+  return output;
 }
 
-got('http://developers.agenciaideias.com.br/correios/rastreamento/json/' + command, {json: true})
-  .then(function (response) {
-    if (Array.isArray(response.body)) {
-      response.body.forEach(function (data) {
-        parse(data);
-      });
-    } else {
-      console.log(chalk.red.bold('Houve algum erro, seu código está correto? ' + command));
-    }
-  })
-  .catch(function () {
-    console.log(chalk.red.bold('Houve algum erro, seu código está correto? ' + command));
-    process.exit(1);
-  });
+
+function fetchTracking ({ url, userInput}) {
+  got(url + userInput, { json: true })
+    .then(function (response) {
+      if (!response.body[0].erro) {
+        let lastOnBot = response.body[0].evento.reverse();
+
+        lastOnBot.forEach(function (data) {
+          process.stdout.write(parse(data));
+        });
+
+      } else {
+        console.log(chalk.red.bold('Erro! ' + response.body[0].erro));
+      }
+    })
+    .catch(function (err) {
+      if (cli.flags.verbose) console.error(err);
+      console.log(chalk.red.bold('Erro!'));
+      process.exit(1);
+    });
+}
+
+fetchTracking({ url: CORREIOS_SERVICE_URL, userInput: command })
